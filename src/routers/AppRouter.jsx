2c8b57c7d4ded250fcd70d4d126b975b8f7564d1
Joaquim-Frances <quimifrances@gmail.com>
2021-05-18 22:46:25 +0200

    Second Commit Setting Up the App

---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const AppRouter = () => {
+    return (
+        <div>
+            {
+            /* Router:
+            path=/auth
+            NO es exact 
+            component={AuthRouter}*/}
+
+            {/* 
+            Main route
+            exact
+            path=/
+            component={Journal Screen}
+            */}
+        </div>
+    )
+}
---
+++ src/routers/AuthRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const AuthRouter = () => {
+    return (
+        <div>
+            {/* 
+            
+            Router no va
+            switch
+            path= /auth/login
+            component=login screen
+            
+            path= /auth/login
+            component=login screen
+
+            redirect to = "/auth/login"
+            
+            */}
+        </div>
+    )
+}
