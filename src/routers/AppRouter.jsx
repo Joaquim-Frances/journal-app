8592a9447adf9f071f29025e636ee30fdcdfef97
Joaquim-Frances <quimifrances@gmail.com>
2021-05-19 11:35:41 +0200

    Sass styling set up and started

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { JournalScreen } from "../components/journal/JournalScreen";
+import { AuthRouter } from "./AuthRouter";
 
 export const AppRouter = () => {
-    return (
-        <div>
-            {
-            /* Router:
-            path=/auth
-            NO es exact 
-            component={AuthRouter}*/}
-
-            {/* 
-            Main route
-            exact
-            path=/
-            component={Journal Screen}
-            */}
-        </div>
-    )
-}
+  return (
+    <Router>
+      <div>
+          <Switch>
+              <Route path="/auth" component={AuthRouter} />
+              <Route exact path="/" component={JournalScreen} />
+              <Redirect to="/auth/login" />
+          </Switch>
+      </div>
+    </Router>
+  );
+};
